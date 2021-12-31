d2dc1b91add9b35246e9942c6db4deaa5573deb1
franco <x.francoruiz@gmail.com>
2021-12-31 03:24:43 -0300

    working in createUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,34 @@
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const { off } = require("../models/User");
 
 exports.createUser = async (request, response) => {
+  // verify errors
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return response.status(400).json({ errors: errors.array() });
+
+  // extract email and password
+  const { email, password } = request.body;
+
   try {
-    let user;
+    let user = await User.findOne({ email });
+
+    if (user) return response.status(400).json({ msg: "This user exist" });
+
     // create user
     user = new User(request.body);
+
+    // password hash
+    const salt = await bcryptjs.genSaltSync(10);
+    user.password = await bcryptjs.hash(password, salt);
+
     // save new user
     await user.save();
-    response.send("User create");
+
+    response.json({ msg: "User create" });
   } catch (error) {
     console.log(error);
     response.status(400).send("Error!!!");
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
+const { check } = require("express-validator");
 
 // create an user
-router.post("/", userController.createUser);
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "email is required").isEmail(),
+  ],
+  userController.createUser
+);
 
 module.exports = router;
